ada41e35f00db46a55afbeba7d7a296ab9608b67
Oleg Solovyov <mcpain@altlinux.org>
2023-12-04 00:59:27 +0300

    Process the whole ray

--- data/shaders/opengl/rayleigh.glsl
+++ data/shaders/opengl/rayleigh.glsl
@@ -81,14 +81,66 @@ float predictDensityInOut(const in vec3 sample, const in vec3 dir, const in vec3
     return opticalDepth;
 }
 
-vec3 computeIncidentLight(const in vec3 sunDirection, const in vec3 dir, const in vec3 center, const in vec2 atmosDist)
+void skipRay(inout vec2 opticalDepth, const in vec3 dir, const in vec2 boundaries, const in vec3 center)
+{
+	int numSamples = 16;
+
+	float tCurrent = boundaries.x;
+	float segmentLength = (boundaries.y - boundaries.x) / numSamples;
+	for (int i = 0; i < numSamples; ++i) {
+		vec3 samplePosition = vec3(tCurrent + segmentLength * 0.5f) * dir;
+
+		// primary ray is approximated by (density * isegmentLength)
+		vec2 density;
+		scatter(density, samplePosition, center);
+		opticalDepth += exp(density) * segmentLength;
+
+		tCurrent += segmentLength;
+	}
+}
+
+void processRay(inout vec3 sumR, inout vec3 sumM, inout vec2 opticalDepth, const in vec3 sunDirection, const in vec3 dir, const in vec2 boundaries, const in vec3 center)
 {
 	vec3 betaR = vec3(3.8e-6f, 13.5e-6f, 33.1e-6f);
 	vec3 betaM = vec3(21e-6f);
 
-	float earthRadius = geosphereRadius,
-	      atmosphereRadius = geosphereRadius * geosphereAtmosTopRad,
-	      atmosphereHeight = atmosphereRadius - earthRadius;
+	int numSamples = 16;
+
+	float atmosphereRadius = geosphereRadius * geosphereAtmosTopRad,
+	      atmosphereHeight = atmosphereRadius - geosphereRadius;
+
+	float tCurrent = boundaries.x;
+	float segmentLength = (boundaries.y - boundaries.x) / numSamples;
+	for (int i = 0; i < numSamples; ++i) {
+		vec3 samplePosition = vec3(tCurrent + segmentLength * 0.5f) * dir;
+
+		vec2 density;
+		scatter(density, samplePosition, center);
+		opticalDepth += exp(density) * segmentLength;
+
+		// light optical depth
+		vec2 opticalDepthLight = vec2(0.f);
+		vec3 samplePositionLight = samplePosition;
+
+		vec3 sampleGeoCenter = center - samplePosition;
+		opticalDepthLight.x = predictDensityInOut(samplePositionLight, sunDirection, sampleGeoCenter, geosphereRadius, atmosphereHeight, coefficientsR);
+		opticalDepthLight.y = predictDensityInOut(samplePositionLight, sunDirection, sampleGeoCenter, geosphereRadius, atmosphereHeight, coefficientsM);
+
+		vec3 tau = -(betaR * (opticalDepth.x + opticalDepthLight.x) + betaM * 1.1f * (opticalDepth.y + opticalDepthLight.y));
+		vec3 tauR = tau + vec3(density.x);
+		vec3 tauM = tau + vec3(density.y);
+		vec3 attenuationR = exp(tauR) * segmentLength;
+		vec3 attenuationM = exp(tauM) * segmentLength;
+		sumR += attenuationR;
+		sumM += attenuationM;
+		tCurrent += segmentLength;
+	}
+}
+
+vec3 computeIncidentLight(const in vec3 sunDirection, const in vec3 dir, const in vec3 center, const in vec2 atmosDist)
+{
+	vec3 betaR = vec3(3.8e-6f, 13.5e-6f, 33.1e-6f);
+	vec3 betaM = vec3(21e-6f);
 
 	float atmosMin = atmosDist.x * geosphereRadius;
 	float atmosMax = atmosDist.y * geosphereRadius;
@@ -120,11 +172,8 @@ vec3 computeIncidentLight(const in vec3 sunDirection, const in vec3 dir, const i
 	float rayMin = atmosMin;
 	float rayMax = atmosMax;
 	if (intersectsShadow) {
-		float new_tmin = min(atmosMax, cylinder_intersect.y);
-		float new_tmax = max(atmosMin, cylinder_intersect.x);
-
-		rayMin = startsInside ? new_tmin : atmosMin;
-		rayMax = startsInside ? atmosMax : new_tmax;
+		rayMin = min(atmosMax, cylinder_intersect.y);
+		rayMax = max(atmosMin, cylinder_intersect.x);
 	}
 
 	if (rayMin == rayMax)
@@ -135,47 +184,20 @@ vec3 computeIncidentLight(const in vec3 sunDirection, const in vec3 dir, const i
 	vec3 sumM = vec3(0.0); // mie and rayleigh contribution
 
 	vec2 opticalDepth = vec2(0.f);
+
+	if (atmosMin != rayMax) {
+		// process near side of ray
+		processRay(sumR, sumM, opticalDepth, sunDirection, dir, vec2(atmosMin, rayMax), center);
+	}
+
 	if (atmosMin != rayMin) {
-		float iCurrent = atmosMin;
-		float isegmentLength = (rayMin - atmosMin) / numSamples;
-		for (int i = 0; i < numSamples; ++i) {
-			vec3 samplePosition = vec3(iCurrent + isegmentLength * 0.5f) * dir;
-
-			// primary ray is approximated by (density * isegmentLength)
-			vec2 density;
-			scatter(density, samplePosition, center);
-			opticalDepth += exp(density) * isegmentLength;
-
-			iCurrent += isegmentLength;
-		}
+		// calculate optical depth for shadow
+		skipRay(opticalDepth, dir, vec2(rayMax, rayMin), center);
 	}
 
-	float tCurrent = rayMin;
-	float segmentLength = (atmosMax - rayMin) / numSamples;
-	for (int i = 0; i < numSamples; ++i) {
-		vec3 samplePosition = vec3(tCurrent + segmentLength * 0.5f) * dir;
+	// process far side of ray
+	processRay(sumR, sumM, opticalDepth, sunDirection, dir, vec2(rayMin, atmosMax), center);
 
-		vec2 density;
-		scatter(density, samplePosition, center);
-		opticalDepth += exp(density) * segmentLength;
-
-		// light optical depth
-		vec2 opticalDepthLight = vec2(0.f);
-		vec3 samplePositionLight = samplePosition;
-
-		vec3 sampleGeoCenter = center - samplePosition;
-		opticalDepthLight.x = predictDensityInOut(samplePositionLight, sunDirection, sampleGeoCenter, earthRadius, atmosphereHeight, coefficientsR);
-		opticalDepthLight.y = predictDensityInOut(samplePositionLight, sunDirection, sampleGeoCenter, earthRadius, atmosphereHeight, coefficientsM);
-
-		vec3 tau = -(betaR * (opticalDepth.x + opticalDepthLight.x) + betaM * 1.1f * (opticalDepth.y + opticalDepthLight.y));
-		vec3 tauR = tau + vec3(density.x);
-		vec3 tauM = tau + vec3(density.y);
-		vec3 attenuationR = exp(tauR) * segmentLength;
-		vec3 attenuationM = exp(tauM) * segmentLength;
-		sumR += attenuationR;
-		sumM += attenuationM;
-		tCurrent += segmentLength;
-	}
 
 	float mu = dot(dir, sunDirection); // mu in the paper which is the cosine of the angle between the sun direction and the ray direction
 	float phaseR = 3.f / (16.f * 3.141592) * (1 + mu * mu);
